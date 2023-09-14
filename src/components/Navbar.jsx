81fb5a2537d1ee841b4ff6cd27396f1b5287106a
Paul Sisson <paulsissonsemail@gmail.com>
2023-09-14 17:56:17 -0500

    Add google, facebook, and github sign in buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,13 @@
+"use client"
+
 import Image from "next/image";
 import Logo from '../resources/logo.svg';
-import { Amaranth } from 'next/font/google'
+import { Amaranth } from 'next/font/google';
+import ReactModal from "react-modal";
+// ReactModal.setAppElement('#navbar')
+import { useState } from "react";
+
+
 
 const font = Amaranth({ 
   weight: ['400'],
@@ -8,32 +15,78 @@ const font = Amaranth({
 })
 
 export default function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  }
+
+  const closeModal = () => {
+    setIsOpen(false);
+  }
+
   return (
-    <div className={`${font.className} h-full max-h-[100px] border-b-2 border-amber-700/90 bg-emerald-900/90 mb-2 md:p-2 flex items-start justify-between w-full`}>
+    <div id="navbar" className={`${font.className} h-full max-h-[100px] border-b-2 border-amber-700/90 bg-emerald-900/90 mb-2 md:p-2 flex items-start justify-between w-full`}>
       <a href="/" className="flex flex-col-reverse md:flex-row items-start md:items-end p-1 w-1/2 text-slate-200 text-xl overline">
         <Image src={Logo} alt='logo' width={80} height={80} />
         <span className="flex">          
           Everybody <span className="text-rose-600">  &#9825; </span>&apos;s Pizza
         </span>
       </a>
-      {/* hidden on medium screens+ */}
+      {/* for under 768px/mobile */}
       <div className="md:hidden text-slate-200 w-1/3 h-full flex items-center justify-end mr-2">
-        <button className="p-1 text-2xl border-thin border-black rounded-md bg-emerald-900/90">&#9776;</button>
+        <button onClick={openModal} className="px-2 text-2xl border-thin border-black rounded-md bg-emerald-900/90">
+          {isOpen === true ? <span>X</span> : <span>&#9776;</span>}
+          </button>
       </div>
-      {/* hidden on mobile */}
+      {/* for 768px+ */}
       <div className="hidden md:flex items-end justify-evenly w-1/3 h-full text-xl text-slate-200">
         <a className="" href="/about">
-          <h3>About</h3>
+          <h3>Shop</h3>
         </a>
         <a className="" href="/shop">
-          <h3>Shop</h3>
+          <h3>Contact</h3>
         </a>
         <a href="/sign-in">
           <h3>Sign in</h3>
         </a>
       </div>
-      
 
+      <ReactModal
+        isOpen={isOpen}
+        contentLabel={'site navigation menu'}
+        onRequestClose={closeModal}
+        style={{
+          content: {
+            width: '275px',
+            backgroundColor: 'rgb(180 83 9 / 0.7)',
+            border: '3px solid white',
+            borderRadius: '15px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          },
+          overlay: {
+            backgroundImage: 'linear-gradient(45deg, transparent 0% 48%, #065f46 48% 52%, transparent 52% 100%',
+            backgroundSize: '10% 10%',
+            backgroundRepeat: 'repeat',            
+          }
+        }}
+      >
+        <div className={`${font.className} flex flex-col items-start justify-evenly w-full h-full text-5xl text-slate-200`}>
+        <a className=" from-emerald-950 via-emerald-800 to-transparent bg-gradient-to-l border-t-thin border-b-thin border-r-thin border-white px-4 py-2 rounded-r-md" href="/about">
+          <h3>Shop</h3>
+        </a>
+        <a className="self-end from-emerald-950  via-emerald-800  to-transparent bg-gradient-to-r border-t-thin border-b-thin border-l-thin border-white px-4 py-2 rounded-l-md" href="/shop">
+          <h3>Contact</h3>
+        </a>
+        <a className="from-emerald-950  via-emerald-800  to-transparent bg-gradient-to-l border-t-thin border-b-thin border-r-thin border-white px-4 py-2 rounded-r-md" href="/sign-in">
+          <h3>Sign in</h3>
+        </a>
+      </div>
+      <button onClick={closeModal} className='bg-slate-100/70 text-slate-950 text-xl w-full p-2 rounded-md border-thin border-black'>Close</button>
+      </ReactModal>
     </div>
   )
 }
---
+++ src/utilities/firebase.utils.js
@@ -0,0 +1,90 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, GithubAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQpkHFSW9w0OfEpTvwHL7LMTHRv1VexNs",
+  authDomain: "e-commerce-site-fb6e3.firebaseapp.com",
+  projectId: "e-commerce-site-fb6e3",
+  storageBucket: "e-commerce-site-fb6e3.appspot.com",  ////given directly from firebase
+  messagingSenderId: "535965629358",
+  appId: "1:535965629358:web:371d2d5a7bb96e54f4e63f"
+};
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: "select_account"
+})
+
+facebookProvider.setCustomParameters({
+  'display': 'popup'
+});
+
+//creating an auth instance
+export const auth = getAuth();
+auth.useDeviceLanguage();
+
+//setting up auth providers
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithFacebookPopup = () => signInWithPopup(auth, facebookProvider);
+export const signInWithGitHubPopup = () => signInWithPopup(auth, githubProvider);
+
+//creating a firestore db instance
+export const db = getFirestore();
+
+
+export const createUserDocFromAuth = async (user) => {
+
+  //doc() takes in the firestore instance, 'const db', from above
+  //then a 'collection' name (essentially a table name in SQL)
+  //then a 'path segment'. Here it is user.uid. The user param is
+  //coming from signInWithPopup() which returns the signed
+  //in user along with the provider's credential
+  //** user, if I was signing in, would be Paul Sisson
+  //and then google's version of me. (email, my uid in their system, etc)**
+  const userDocRef = doc(db, 'users', user.uid)
+  
+  //so basically userDocRef ^^^ is a query statement and it returns 
+  //what you've asked for from the location you specify
+  //in our case:
+  // -- db (our Firestore database from our getFirestore() call)
+  // -- 'users' (the SQL equivalent of table name)
+  // -- user.uid (the SQL equivalent of WHERE id = user.id)
+
+  //getDoc() is basically a query to the database to see
+  //if a user with that uid exists already
+  //if not, the function returns an error
+  const userSnapshot = await getDoc(userDocRef);
+
+  //here we are saying if userSnapshot returns an error
+  //create a new user in our database from the information
+  if(!userSnapshot.exists()) {
+    const { displayName, email } = user;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName: displayName,
+        email: email,
+        createdAt: createdAt
+      });
+    } catch (err) {
+      console.error('error creating user' , err.message)
+    }
+  }
+
+  return userDocRef;
+}
+
+
+
+
+
+
+
